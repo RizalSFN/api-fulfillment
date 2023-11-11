fc23138bd616c69820746aa378f66f89b0dfc893
RizalS <87477682+RizalSFN@users.noreply.github.com>
2023-11-11 21:47:11 +0700

    modify success response

--- src/response/success-response.js
+++ src/response/success-response.js
@@ -1,16 +1,31 @@
 const http = require("http");
-const successResponse = (statusCode, data, msg, res) => {
-  res.status(statusCode).json([
-    {
-      message: msg,
-      note: "Success",
-      payload: data,
-      status_code: {
-        code: statusCode,
-        status: http.STATUS_CODES[statusCode],
+const successResponse = (statusCode, data, msg, res, pagination) => {
+  // if (pagination || pagination !== undefined) {
+    res.status(statusCode).json([
+      {
+        message: msg,
+        note: "Success",
+        payload: data,
+        status_code: {
+          code: statusCode,
+          status: http.STATUS_CODES[statusCode],
+        },
+        metadata: (pagination) ? pagination : "",
       },
-    },
-  ]);
+    ]);
+  // // } else {
+  //   res.status(statusCode).json([
+  //     {
+  //       message: msg,
+  //       note: "Success",
+  //       payload: data,
+  //       status_code: {
+  //         code: statusCode,
+  //         status: http.STATUS_CODES[statusCode],
+  //       },
+  //     },
+  //   ]);
+  // }
 };
 
 module.exports = successResponse;
